6df3fbf2a264098aa967628bb85f45fec3fca9c6
Jesus Salazar <jesus270498@gmail.com>
2019-09-25 19:17:20 -0500

    Change index php

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<?php echo 'Hello world!'; ?>
+<?
+	$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
+	$user = "ryvfvynzqazlcx";
+	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
+	$dbname = "d1jv2gq7sc413i";
+	$port = "5432";
+	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
+ 
+	// establecemos una conexion con el servidor postgresSQL
+	$dbconn = pg_connect($conn_string);
+	// Revisamos el estado de la conexion en caso de errores. 
+	if(!$dbconn) {
+		echo "Error: No se ha podido conectar a la base de datos\n";
+	} 
+	else 
+	{
+		echo "Conexion exitosa!!\n";
+	}
+
+	$dato1 = $_GET["dato1"];
+	$dato2 = $_GET["dato2"];
+	$temperatura = $_GET["temperatura"];
+	
+	
+	if($temperatura!=null)
+	{
+		$query ="INSERT INTO tbl_datos (temperatura,humedad, tiempo) VALUES (".$dato1.",".$dato2.",'".$temperatura."')";
+		
+		pg_query($query) or die('Error: ' . pg_last_error());
+		
+		echo "Elemento guardados"
+		console.log("Elemento guardados")	
+	}
+	console.log("Test")	
+	// Close connection
+	pg_close($dbconn);
+
+
+?>
