be821ac5962e1b5873cc32ff43d356e27259daf4
Jesus Salazar <jesus270498@gmail.com>
2019-09-25 19:28:38 -0500

    Changes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?
-	$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
+	echo "Test\n";
+	/*$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
 	$user = "ryvfvynzqazlcx";
 	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
 	$dbname = "d1jv2gq7sc413i";
@@ -34,7 +35,7 @@
 	}
 	console.log("Test")	
 	// Close connection
-	pg_close($dbconn);
+	pg_close($dbconn);*/
 
 
 ?>
