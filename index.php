cf72a2ccbea7f7ba8fe7585a7879bad462def327
Jesus Salazar <jesus270498@gmail.com>
2019-09-25 19:57:59 -0500

    Changes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-	echo "Testing";
+	echo "Testing\n";
 	$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
 	$user = "ryvfvynzqazlcx";
 	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
@@ -30,10 +30,9 @@
 		
 		pg_query($query) or die('Error: ' . pg_last_error());*/
 		
-		echo "Elemento guardados"
-		console.log("Elemento guardados")	
+		echo "Elemento guardados\n";
 	}
-	console.log("Test")	
+	echo "Test\n";
 	// Close connection
 	//pg_close($dbconn);
 
