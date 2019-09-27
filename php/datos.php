e3c8309fcca1360208dc0a05379006ae2e218e7d
Jesus Salazar <jesus270498@gmail.com>
2019-09-26 19:58:17 -0500

    Changes

--- php/datos.php
+++ php/datos.php
@@ -20,7 +20,7 @@ $datos = array(
 //Devolvemos el array pasado a JSON como objeto
 echo json_encode($datos, JSON_FORCE_OBJECT);
 
-    /*$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
+    $host = "ec2-54-83-201-84.compute-1.amazonaws.com";
 	$user = "ryvfvynzqazlcx";
 	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
 	$dbname = "d1jv2gq7sc413i";
@@ -55,5 +55,5 @@ echo json_encode($datos, JSON_FORCE_OBJECT);
 	//echo json_encode($resutado); 
 		 
 	// Close connection
-	pg_close($dbconn);*/
+	pg_close($dbconn);
 ?>
