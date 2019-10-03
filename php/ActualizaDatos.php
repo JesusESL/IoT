7a98b66b01daedc485af591f3d2e331d2b5e91ac
Jesus Salazar <jesus270498@gmail.com>
2019-10-03 16:13:31 -0500

    Changes

--- php/ActualizaDatos.php
+++ php/ActualizaDatos.php
@@ -1,4 +1,4 @@
-<?
+<?php
 	$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
 	$user = "ryvfvynzqazlcx";
 	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
@@ -6,49 +6,24 @@
 	$port = "5432";
 	
 	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
- 
-	// establecemos una conexion con el servidor postgresSQL
 	$dbconn = pg_connect($conn_string);
 	 
-	// Revisamos el estado de la conexion en caso de errores. 
 	if(!$dbconn) {
 		echo "Error: No se ha podido conectar a la base de datos\n";
 	} 
-	else 
-	{
+	else {
 		echo "Conexion exitosa!!\n";
 	}
-	 
-	
-	$temperatura = $_GET["temperatura"];
-	
+
+	$temperature = $_GET["temperature"];
+	$humidity = $_GET["humidity"];
 	date_default_timezone_set('America/Mexico_City');
-	$tiempo = date("Y-m-d H:i:s");
-	
-	
-	if($temperatura!=null)
-	{
-		$query ="UPDATE test_table SET temperatura = ".$temperatura." WHERE id = 42";
-		
-		
-		
+	$date = date("Y-m-d");
+
+	if($temperatura != null){
+		$query =" UPDATE iot_table SET temperature = ".$temperature.", humidity = ".$humidity.", date = ".$date." WHERE id = 1";
 		pg_query($query) or die('Error: ' . pg_last_error());
-		
 		echo "Dato Actualizado";
-		
 	}
-	 
-	
-	//http_response_code(200)
-	//header("Content-type:application/json");
-	 
-	// codificar la respuesta en formato JSON
-	//echo json_encode($resutado); 
-		 
-		 
-	 
-	// Close connection
 	pg_close($dbconn);
-
-
 ?>
--- php/base_datos.php
+++ php/base_datos.php
@@ -1,56 +1,32 @@
 <?php
-
-	$temperatura = 0;
-	
 	$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
 	$user = "ryvfvynzqazlcx";
 	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
 	$dbname = "d1jv2gq7sc413i";
 	$port = "5432";
 	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
-	// establecemos una conexion con el servidor postgresSQL
 	$conexion = pg_connect($conn_string);
-	 
-	// Revisamos el estado de la conexion en caso de errores. 
+
 	if($conexion) {
-		
-		
-		
-		$sql = "SELECT * FROM test_table WHERE id=42";
-		// Ejecutar la consulta:
+		$sql = "SELECT * FROM iot_table WHERE id=1";
 		$rs = pg_query( $conexion, $sql );
-		if( $rs )
-		{
-		// Obtener el número de filas:
-			 if( pg_num_rows($rs) >= 0 )
-			{
-				// Recorrer el resource y mostrar los datos:
-				 while( $obj = pg_fetch_object($rs) )
-					 
-					 $temperatura = $obj->temperatura;
+		if( $rs ){
+			if( pg_num_rows($rs) >= 0 ){
+				while( $obj = pg_fetch_object($rs) )
+					$temperature = $obj->temperature;
+					$humidity = $obj->humidity;
+					$temperature = $obj->date;
 			}
-			
 		}
-		
 	} 
 
-
-
-//Obtenemos los datos de los input
-$opcion = $_POST["opcion"];
-
-//Hacemos las comprobaciones que sean necesarias... (sanitizar los textos para evitar XSS e inyecciones de código, comprobar que la edad sea un número, etc.)
-//Omitido para la brevededad del código
-//PERO NO OLVIDES QUE ES ALGO IMPORTANTE.
-
-//Seteamos el header de "content-type" como "JSON" para que jQuery lo reconozca como tal
-header('Content-Type: application/json');
-//Guardamos los datos en un array
-$datos = array(
-'estado' => 'ok',
-
-'temperatura' => $temperatura
-);
-//Devolvemos el array pasado a JSON como objeto
-echo json_encode($datos, JSON_FORCE_OBJECT);
+	$opcion = $_POST["opcion"];
+	header('Content-Type: application/json');
+	$datos = array(
+		'estado' => 'ok',
+		'temperatura' => $temperatura,
+		'humidity' => $humidity,
+		'date' => $date
+	);
+	echo json_encode($datos, JSON_FORCE_OBJECT);
 ?>
