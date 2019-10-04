c5c060a5a1e61914101f5179bbb6688fd64156b9
Jesus Salazar <jesus270498@gmail.com>
2019-10-03 19:17:31 -0500

    Changes

--- php/ActualizaDatos.php
+++ php/ActualizaDatos.php
@@ -4,7 +4,6 @@
 	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
 	$dbname = "d1jv2gq7sc413i";
 	$port = "5432";
-	
 	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
 	$dbconn = pg_connect($conn_string);
 	 
--- php/base_datos.php
+++ php/base_datos.php
@@ -10,15 +10,13 @@
 	if($conexion) {
 		$sql = "SELECT * FROM iot_table WHERE id=1";
 		$rs = pg_query( $conexion, $sql );
+		console.log($rs)
 		if( $rs ){
 			if( pg_num_rows($rs) >= 0 ){
 				while( $obj = pg_fetch_object($rs) )
 					$temperature = $obj->temperature;
 					$humidity = $obj->humidity;
 					$date = $obj->date;
-					console.log(temperature);
-					console.log(humidity);
-					console.log(date);
 			}
 		}
 	} 
