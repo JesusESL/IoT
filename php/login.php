6b93e56080b367facbd868572cd4966a73ad10cf
Jesus Salazar <jesus270498@gmail.com>
2019-10-06 18:15:16 -0500

    Changes

--- php/login.php
+++ php/login.php
@@ -1,5 +1,4 @@
 <?php
-    echo "Hello world!\n";
     $host = "ec2-54-83-201-84.compute-1.amazonaws.com";
     $user = "ryvfvynzqazlcx";
     $password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
@@ -19,11 +18,12 @@
                 while( $obj = pg_fetch_object($rs) ){
                     $username = $obj->username;
                     $pass = $obj->pass;
-                    console.log($username);
-                    console.log($pass);
                 }
             }
         }
+        echo "Conecxion\n";
+    } else {
+        echo "No conecxion\n";
     }
     
     $opcion = $_POST["opcion"];
