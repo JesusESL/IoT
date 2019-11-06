296aaf51cbf04d6cf420ab35d6bd9da9e6668ad0
Jesus Salazar <jesus270498@gmail.com>
2019-11-06 12:23:24 -0600

    Changes

---
+++ php/getData.php
@@ -0,0 +1,38 @@
+<?php
+	$host = "ec2-54-83-201-84.compute-1.amazonaws.com";
+	$user = "ryvfvynzqazlcx";
+	$password = "54073c0f267ca0ace145876ac7d4d981064d8c9f1af3efd503512a629ec800b7";
+	$dbname = "d1jv2gq7sc413i";
+	$port = "5432";
+	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
+	$conexion = pg_connect($conn_string);
+
+	$temperature = 0;
+  $humidity = 0;
+  $date = 0;
+
+	if($conexion) {
+		$sql = "SELECT * FROM iot_table WHERE id=1";
+		$rs = pg_query( $conexion, $sql );
+		if( $rs ){
+			if( pg_num_rows($rs) >= 0 ){
+				while( $obj = pg_fetch_object($rs) ){
+					$temperature = $obj->temperature;
+					$humidity = $obj->humidity;
+          $date = $obj->date;
+				}
+			}
+		}
+	} 
+
+	$opcion = $_POST["opcion"];
+	header('Content-Type: application/json');
+	$datos = array(
+		'estado' => 'ok',
+    'temperatura' => $temperature,
+    'humedad' => $humidity,
+    'fecha' => $date
+	);
+    echo json_encode($datos, JSON_FORCE_OBJECT);
+
+?>
