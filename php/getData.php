08c8afa4178edf47443f00b77e0f739ea5ba4501
Jesus Salazar <jesus270498@gmail.com>
2019-12-04 12:29:59 -0600

    Changes

--- php/getData.php
+++ php/getData.php
@@ -7,13 +7,6 @@
 	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
 	$dbconn = pg_connect($conn_string);
 
-	if(!$dbconn) {
-		echo "Error: No se ha podido conectar a la base de datos\n";
-	} 
-	else {
-		echo "Conexion exitosa!!\n";
-	} 
-
 	$data[] = [];
 
 	if($dbconn) {
--- php/postData.php
+++ php/postData.php
@@ -6,13 +6,6 @@
 	$port = "5432";
 	$conn_string = "host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$password." options='--client_encoding=UTF8'";
 	$dbconn = pg_connect($conn_string);
-	 
-	if(!$dbconn) {
-		echo "Error: No se ha podido conectar a la base de datos\n";
-	} 
-	else {
-		echo "Conexion exitosa!!\n";
-	}
 
 	$id = $_GET["id"];
 	$temperature = $_GET["temperature"];
