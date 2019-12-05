9d0d96748012cd8eb92d59cafc2503ba3bd619c6
Jesus Salazar <jesus270498@gmail.com>
2019-12-05 11:38:38 -0600

    Changes

--- js/create_user.js
+++ js/create_user.js
@@ -1,3 +1,9 @@
-$( "#createUser" ).click(function() {
-  alert( "Handler for .click() called." );
+$('##createUser').submit(function() {
+  var username = $("input[name=name]").val();
+  var email = $("input[name=email]").val();
+  var sensor_id = $("input[name=id]").val();
+
+  console.log(username);
+  console.log(email);
+  console.log(sensor_id);
 });
