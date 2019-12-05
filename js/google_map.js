820a3962f22f5839b0e94b062c57781e079d4c0e
Jesus Salazar <jesus270498@gmail.com>
2019-12-04 18:55:43 -0600

    Changes

--- js/google_map.js
+++ js/google_map.js
@@ -9,6 +9,8 @@ var lng = [];
 var id = [];
 var temperature = [];
 var humidity = [];
+var showHumidity = false;
+var showTemperature = false;
 
 $(function() {
     map = new GMaps({
@@ -74,15 +76,31 @@ window.setInterval(
             }
 
             for(var i = 0; i < dataSize; i++){
-                if(temperature[i] < 19){
-                    circles[i].set('fillColor','#00FF00');
-                    circles[i].set('strokeColor','#00FF00');
-                } else if(temperature[i] > 20 && temperature[i] < 29){
-                    circles[i].set('fillColor','#F0FF00');
-                    circles[i].set('strokeColor','#F0FF00');
-                } else if(temperature[i] > 30){
-                    circles[i].set('fillColor','#FF0000');
-                    circles[i].set('strokeColor','#FF0000');
+                if(showTemperature){
+                    if(temperature[i] < 19){
+                        circles[i].set('fillColor','#00FF00');
+                        circles[i].set('strokeColor','#00FF00');
+                    } else if(temperature[i] > 20 && temperature[i] < 29){
+                        circles[i].set('fillColor','#F0FF00');
+                        circles[i].set('strokeColor','#F0FF00');
+                    } else if(temperature[i] > 30){
+                        circles[i].set('fillColor','#FF0000');
+                        circles[i].set('strokeColor','#FF0000');
+                    }
+                }
+                if(showHumidity){
+                    for(var i = 0; i < dataSize; i++){
+                        if(humidity[i] < 59){
+                            circles[i].set('fillColor','#bbdefb');
+                            circles[i].set('strokeColor','#bbdefb');
+                        } else if(humidity[i] > 60 && humidity[i] < 79){
+                            circles[i].set('fillColor','#42a5f5');
+                            circles[i].set('strokeColor','#42a5f5');
+                        } else if(humidity[i] > 80){
+                            circles[i].set('fillColor','#1565c0');
+                            circles[i].set('strokeColor','#1565c0');
+                        }
+                    }
                 }
             }
         }
@@ -90,31 +108,11 @@ window.setInterval(
 ,1000);
 
 $('#changeTemperature').click(function() {
-    for(var i = 0; i < dataSize; i++){
-        if(temperature[i] < 19){
-            circles[i].set('fillColor','#00FF00');
-            circles[i].set('strokeColor','#00FF00');
-        } else if(temperature[i] > 20 && temperature[i] < 29){
-            circles[i].set('fillColor','#F0FF00');
-            circles[i].set('strokeColor','#F0FF00');
-        } else if(temperature[i] > 30){
-            circles[i].set('fillColor','#FF0000');
-            circles[i].set('strokeColor','#FF0000');
-        }
-    }
+    showTemperature = true;
+    showHumidity = false;
 });
 
 $('#changeHumidity').click(function() {
-    for(var i = 0; i < dataSize; i++){
-        if(humidity[i] < 59){
-            circles[i].set('fillColor','#bbdefb');
-            circles[i].set('strokeColor','#bbdefb');
-        } else if(humidity[i] > 60 && humidity[i] < 79){
-            circles[i].set('fillColor','#42a5f5');
-            circles[i].set('strokeColor','#42a5f5');
-        } else if(humidity[i] > 80){
-            circles[i].set('fillColor','#1565c0');
-            circles[i].set('strokeColor','#1565c0');
-        }
-    }
+    showTemperature = false;
+    showHumidity = true;
 });
