4d2354dac3ae11c7f4c37e647f4b33ed86120d1d
Jesus Salazar <jesus270498@gmail.com>
2019-12-05 11:53:57 -0600

    Changes

--- js/create_user.js
+++ js/create_user.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $('#basicform').submit(function(event){
       event.preventDefault();
+
+      var username = $("#inputUserName").val();
+      var email = $("#inputEmail").val();
+      var id = $("#inputID").val();
+
+      console.log(username);
   });
 });
